ccc0adec7986089edd2902922971e486649241a4
vbabulreddy <in.babulv@gmail.com>
2022-09-13 19:59:52 +0530

    Terraform script

---
+++ Rg.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "Rg1" {
+    name = "Devops-TF1"
+    location = "East Us"
+
+  
+}
+
+#resource "azurerm_resource_group" "Rg2" {
+#  name     = "example"
+#  location = "West Europe"
+#}
---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.21.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+    features {}
+
+  subscription_id = "810cf854-b0a9-4ad1-a9ce-438024596053"
+  client_id       = "7b54bd92-4561-45be-ab10-c0510ab0917b"
+  client_secret   = "Nmr8Q~J9UiffjbdLKiFPh34QJ8vCXLY6q-kVqad2"
+  tenant_id       = "c2e6ffd9-4eef-4fe6-af0b-a9a0846fb515"
+}
